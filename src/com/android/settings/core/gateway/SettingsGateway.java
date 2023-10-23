f6780a6c003858126b19aab1969c39419bfedd85
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-10-23 06:55:19 +0530

    Settings: Export intent for smooth display settings
    Change-Id: I1be39f0134525253597fbf04701ecc40748cab3b
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -105,6 +105,7 @@ import com.android.settings.display.AutoBrightnessSettings;
 import com.android.settings.display.DcDimmingSettings;
 import com.android.settings.display.NightDisplaySettings;
 import com.android.settings.display.SmartAutoRotatePreferenceFragment;
+import com.android.settings.display.SmoothDisplayFragment;
 import com.android.settings.display.darkmode.DarkModeSettingsFragment;
 import com.android.settings.dream.DreamSettings;
 import com.android.settings.enterprise.EnterprisePrivacySettings;
